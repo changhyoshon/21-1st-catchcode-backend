e06f72c25cef2641cd5d7e45848f078e32945da5
seungwon <seungwon@bu.edu>
2021-06-11 18:26:07 +0900

    MODIFIED: urls.py 수정, QuerySet대응 서버 수정

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ProductDetails
+from .views import ProductDetails, ProductCategories
 urlpatterns = [
-  path('/details/<products_id>', ProductDetails.as_view())
+  path('/<int:products_id>', ProductDetails.as_view()),
+  path('/categories', ProductCategories.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -1,17 +1,37 @@
 from django.http  import JsonResponse
 from django.views import View
 
-from .models import Product, ProductSize, Image, ProductContent
+from .models import Product, ProductSize, Image, ProductContent, Category, Country
+
+class ProductCategories(View):
+  def get(self, request):
+    result = {
+      'categories' : [
+        {
+          'id' : object.id,
+          'name' : object.name,
+          'imageUrl' : object.image_url
+        } for object in Category.objects.all()
+      ],
+      'countries' : [
+        {
+          'id' : object.id,
+          'name' : object.name,
+          'imageUrl' : object.image_url
+        } for object in Country.objects.all()
+      ]
+    }
+    return JsonResponse({'result' : result}, status=200)
 
 class ProductDetails(View):
     def get(self, request, products_id):
         product          = Product.objects.get(id=products_id)
-        image            = Image.objects.get(product_id=product.id)
+        images           = Image.objects.filter(product_id=product.id)
         product_contents = ProductContent.objects.filter(product_id=product.id)
         product_sizes    = ProductSize.objects.filter(product_id=product.id)
         
         result={
-            'products' :
+            'products' : 
                 {
                     'id'                : product.id,
                     'category'          : product.category.name,
@@ -19,7 +39,7 @@ class ProductDetails(View):
                     'description'       : product.description,
                     'price'             : [{product_size.size.id: product_size.price for product_size in product_sizes}],
                     'size'              : [{product_size.size.id: product_size.stock for product_size in product_sizes}],
-                    'image'             : image.url,
+                    'image'             : [{image.id: image.url for image in images}],
                     'country'           : product.country.name,
                     'color'             : product.color,                 
                     'product_substance' : [{product_content.content.name: product_content.percent for product_content in product_contents}]
