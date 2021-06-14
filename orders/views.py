f21ff434ec57799e9d886b11b96454dfed296e39
yu <yu@yuui-MacBookPro.local>
2021-06-14 20:32:16 +0900

    MODIFY: select_related, prefetch_related 활용하여 리팩토링 완료

--- orders/views.py
+++ orders/views.py
@@ -17,8 +17,8 @@ class OrdersPayment(View):
             datas = json.loads(request.body)
 
             is_paid = Order.objects.filter(
-                id      = datas['orderId'], 
-                user_id = id, 
+                id      = datas['orderId'],
+                user_id = id,
                 status  = 1
             ).update(status_id = 2)
             
@@ -34,37 +34,36 @@ class OrdersPayment(View):
 class OrdersCart(View):
     @LoginStatus
     def get(self, request):
-        id = request.user.id
+        try:
+            id = request.user.id
 
-        order = Order.objects.filter(user_id=id, status_id=1) or False
+            order = Order.objects.get(user_id = id, status_id = 1)
+            
+            order_items = order.orderitem_set.all().select_related('product', 'size').prefetch_related('product__product_size', 'product__image_set')
 
-        if not order:
-            return JsonResponse({'result' : {
-                'orderId'  : -1,
-                'products' : []
-            }}, status=200)
+            result = {
+                'orderId' : order.id,
+                'products':[
+                    {
+                        'productId'   : object.product_id,
+                        'productName' : object.product.name,
+                        'catchCode'   : object.product.catch_code,
+                        'thumbNail'   : object.product.image_set.all().order_by('id').first().url,
+                        'quantity'    : object.quantity,
+                        'totalPrice'  : object.total_price,
+                        'sizeId'      : object.size_id,
+                        'sizeName'    : object.size.name,
+                        'stock'       : object.product.productsize_set.filter(size_id = object.size_id)[0].stock,
+                        'orderItemId' : object.id
+                    } for object in order_items
+                ]
+            }
 
-        order_items = order[0].orderitem_set.all()
-
-        result = {
-            'orderId' : order[0].id,
-            'products':[
-                {
-                    'productId'   : object.product_id,
-                    'productName' : object.product.name,
-                    'catchCode'   : object.product.catch_code,
-                    'thumbNail'   : object.product.image_set.all().order_by('id').first().url,
-                    'quantity'    : object.quantity,
-                    'totalPrice'  : object.total_price,
-                    'sizeId'      : object.size_id,
-                    'sizeName'    : object.size.name,
-                    'stock'       : object.product.productsize_set.filter(size_id = object.size_id)[0].stock,
-                    'orderItemId' : object.id
-                } for object in order_items
-            ]
-        }
-
-        return JsonResponse({'result' : result}, status=200)
+            return JsonResponse({'result' : result}, status=200)
+        except Order.MultipleObjectsReturned:
+            return JsonResponse({'result' : 'INVALID CART'}, status=412)
+        except Order.DoesNotExist:
+            return JsonResponse({'result' : {'orderId'  : -1, 'products' : []}}, status=200)
 
     @LoginStatus
     def post(self, request):
@@ -79,13 +78,14 @@ class OrdersCart(View):
             )
         
             if not created:
-                Order.objects.filter(id = obj.id).update(updated_at = timezone.now())
+                obj.updated_at = timezone.now()
+                obj.save()
             
             order_item = OrderItem.objects.filter(
                 order_id   = obj.id,
                 size_id    = datas['sizeId'],
                 product_id = datas['productId']
-            ) or False
+            ).select_related('product').prefetch_related('product__product_size').first()
         
             if not order_item:
                 price = ProductSize.objects.get(
@@ -101,15 +101,13 @@ class OrdersCart(View):
                     product_id  = datas['productId']
                 ).save()
             else:
-                total_price = ProductSize.objects.get(
-                    product_id = datas['productId'], 
-                    size_id    = datas['sizeId']
-                ).price * datas['quantity']
+                total_price = order_item.product.productsize_set.filter(size_id = datas['sizeId']).first().price
+                total_price = total_price * datas['quantity']
 
-                OrderItem.objects.filter(id=order_item[0].id).update(
-                    quantity    = order_item[0].quantity + datas['quantity'],
-                    total_price = order_item[0].total_price + total_price
-                )
+                order_item.quantity    = order_item.quantity + datas['quantity']
+                order_item.total_price = order_item.total_price + total_price
+                order_item.save()
+
             return JsonResponse({'result' : 'SUCCESS'}, status=201)
         except KeyError:
             return JsonResponse({'result' : 'INVALID KEY'}, status=400)
@@ -123,9 +121,9 @@ class OrdersCart(View):
         try:
             datas = json.loads(request.body)
 
-            order_item = OrderItem.objects.filter(id = datas['orderItemId'])
-            order_id   = -1 if not len(order_item) else order_item[0].order_id
-            is_deleted = False if not order_item.delete()[0] else True
+            order_item = OrderItem.objects.filter(id = datas['orderItemId']).first()
+            order_id   = -1 if not order_item else order_item.order_id
+            is_deleted = False if not order_item.delete() else True
 
             if is_deleted:
                 order_item_count = OrderItem.objects.filter(order_id = order_id).count()
