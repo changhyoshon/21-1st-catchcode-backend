d10c25ca746229ca5b8346ac715fa7f74af9b51e
seungwon <seungwon@bu.edu>
2021-06-15 15:52:58 +0900

    ADD: 상품리스트 및 필터기능 구현

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ProductDetails, ProductCategories
+
+from .views import ProductDetails, ProductCategories, ProductListInfo
 urlpatterns = [
   path('/<int:products_id>', ProductDetails.as_view()),
-  path('/categories', ProductCategories.as_view())
+  path('/categories', ProductCategories.as_view()),
+  path('/<str:details>/<int:number>', ProductListInfo.as_view())
 ]
+
--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
-from django.http  import JsonResponse
-from django.views import View
+import sys
+
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Sum, Q
 
 from .models import Product, ProductSize, Image, ProductContent, Category, Country
 
@@ -8,20 +11,20 @@ class ProductCategories(View):
     result = {
       'categories' : [
         {
-          'id' : object.id,
-          'name' : object.name,
+          'id'       : object.id,
+          'name'     : object.name,
           'imageUrl' : object.image_url
         } for object in Category.objects.all()
       ],
       'countries' : [
         {
-          'id' : object.id,
-          'name' : object.name,
+          'id'       : object.id,
+          'name'     : object.name,
           'imageUrl' : object.image_url
         } for object in Country.objects.all()
       ]
     }
-    return JsonResponse({'result' : result}, status=200)
+    return JsonResponse({'productCategories' : result}, status=200)
 
 class ProductDetails(View):
     def get(self, request, products_id):
@@ -31,18 +34,59 @@ class ProductDetails(View):
         product_sizes    = ProductSize.objects.filter(product_id=product.id)
         
         result={
-            'products' : 
-                {
                     'id'                : product.id,
+                    'categoryId'        : product.category.id,
                     'category'          : product.category.name,
                     'name'              : product.name,
                     'description'       : product.description,
-                    'price'             : [{product_size.size.id: product_size.price for product_size in product_sizes}],
-                    'size'              : [{product_size.size.id: product_size.stock for product_size in product_sizes}],
-                    'image'             : [{image.id: image.url for image in images}],
                     'country'           : product.country.name,
-                    'color'             : product.color,                 
-                    'product_substance' : [{product_content.content.name: product_content.percent for product_content in product_contents}]
-                }
+                    'countryId'         : product.country.id,
+                    'color'             : product.color,    
+                    'priceAndSize'      : [{'sizeId': product_size.size.id, 'sizeName' : product_size.size.name, 'price' : product_size.price, 'stock' : product_size.stock} for product_size in product_sizes],
+                    'image'             : [image.url for image in images],  
+                    'productSubstance'  : [{'name' : product_content.content.name, 'value': product_content.percent} for product_content in product_contents]
         }
-        return JsonResponse({'result' : result}, status=200)
+        return JsonResponse({'productDetails' : result}, status=200)
+
+class ProductListInfo(View):
+    def get(self, request, details, number):       
+        catch        = request.GET.get('catch', None)
+        color        = request.GET.get('color', None)
+        price_max    = request.GET.get('priceMax', sys.maxsize)
+        price_min    = request.GET.get('priceMin', 0)
+
+        q = Q()
+
+        if details == 'country':
+            q.add(Q(country_id=number), q.AND)
+            
+        if details == 'category':
+            a = Category.objects.get(id=number).name
+            q.add(Q(category_id=number), q.AND)
+            q.add(Q(category_id=6, name__istartswith=a), q.OR)
+
+        if catch:
+            q.add(Q(catch_code=catch), q.AND)
+
+        if color:
+            q.add(Q(color = color), q.AND)
+
+        q.add(Q(productsize__price__range=(price_min, price_max)), q.AND)
+        q.add(Q(productsize__size_id=3), q.AND)
+          
+        result = [
+            {
+                'id'         : product.id,
+                'name'       : product.name,
+                'catchCode'  : product.catch_code,
+                'countryId'  : product.country.id,
+                'categoryId' : product.category.id,
+                'price'      : product.productsize_set.filter(size_id=3).first().price, #size_id=3 이 가장 저렴한 small size 입니다
+                'thumbNail'  : product.image_set.filter(product_id=product.id).first().url,
+                'stock'      : product.productsize_set.filter(product_id=product.id).aggregate(Sum('stock'))['stock__sum']
+    
+            } for product in Product.objects.filter(q) 
+        ]
+        return JsonResponse({'productListInfo' : result}, status=200)
+
+            
