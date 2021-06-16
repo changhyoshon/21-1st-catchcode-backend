2911c6c56423eeca4050fe5cababda922de374e7
손창효 <ckdgy123@gmail.com>
2021-06-16 14:36:10 +0900

    MODIFY: 컨벤션수정

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .views import ProductDetails, ProductCategories, ProductListInfo, ProductList
+
 urlpatterns = [
   path('/<int:products_id>', ProductDetails.as_view()),
   path('/categories', ProductCategories.as_view()),
   path('/search', ProductListInfo.as_view()),
-  path('',ProductList.as_view())
+  path('', ProductList.as_view())
 ]
 
